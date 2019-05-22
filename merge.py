a704b3c9dc8ad128ef1321771b6d2012d4bc9e54
David Kirkby <dkirkby@uci.edu>
2019-05-22 01:49:34 -0700

    Print shell cmds to merge single night

--- merge.py
+++ merge.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from pathlib import Path
 import yaml
@@ -5,6 +6,11 @@ import json
 
 
 def main():
+    if len(sys.argv) == 2:
+        only_night = int(sys.argv[1])
+        only_night = f'{only_night:08d}'
+    else:
+        only_night = None
     ROOT = Path(os.getenv('SCRATCH')) / 'CI'
     assert ROOT.exists()
     merged = {}
@@ -20,9 +26,20 @@ def main():
             print(f'Merged {len(exposures)} exposures for {night}.')
             merged[night] = exposures
             nexptot += len(exposures)
-    with open('merged.json', 'w') as f:
+    outname = 'merged.js'
+    with open(outname, 'w') as f:
+        print('initnights(', file=f)
         json.dump(merged, f, indent=2)
-    print(f'Merged {nexptot} exposures.')
+        print(');', file=f)
+    print(f'Merged {nexptot} exposures into {outname}.')
+    if only_night:
+        print(f'mkdir $DESI_WWW/users/dkirkby/CI/{only_night}')
+        print(f'cp $SCRATCH/CI/{only_night}/*.jpg $DESI_WWW/users/dkirkby/CI/{only_night}/')
+    print(f'mv {outname} docs/js/')
+    print(f'git add docs/js/{outname}')
+    print(f'git commit -m "Add {only_night}"')
+    print(f'git push')
+
 
 if __name__ == '__main__':
     main()
